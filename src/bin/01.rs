32ce22c8a7d34728c1a2f79905c318bb5edf5983
billo <ateilhet@gmail.com>
2023-12-01 10:39:39 +0100

    day 1 part 1

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,7 +1,30 @@
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
-    None
+    let lines = input.split("\n");
+    let mut r: u32 = 0;
+    for l in lines {
+        r += read_str(l)
+    }
+
+    Some(r)
+}
+
+fn read_str(str: &str) -> u32 {
+    let t: Vec<char> = str
+        .chars()
+        .filter(|c| c.is_digit(10))
+        .collect();
+    let mut binding = t.first().copied();
+    let first = binding.get_or_insert('0');
+    let mut binding_last = t.last().copied();
+    let last = binding_last.get_or_insert('0');
+    let mut r_str = String::new();
+    r_str.push(*first);
+    r_str.push(*last);
+    let r: u32 = r_str.parse().unwrap();
+
+    r
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
@@ -15,7 +38,7 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(142));
     }
 
     #[test]
