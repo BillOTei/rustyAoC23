39fbc897b463f996328d24087c21ae7232e5f335
billo <ateilhet@gmail.com>
2024-01-08 18:24:41 +0100

    day 13 part 2

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -1,4 +1,5 @@
 use array2d::Array2D;
+use itertools::Itertools;
 
 use advent_of_code::get_map;
 
@@ -48,8 +49,72 @@ fn mirrored_at(map: &Vec<Vec<char>>) -> Option<usize> {
     None
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    None
+fn find_smudge(map: &Vec<Vec<char>>) -> Option<usize> {
+    let combinations = map.clone().into_iter().enumerate().combinations(2);
+    let mut all_mirrored_at = vec![];
+    for u in combinations {
+        let a = u[0].clone();
+        let b = u[1].clone();
+        let mut difference: Vec<((usize, usize), char)> = vec![];
+        for (x, tile) in a.1.into_iter().enumerate() {
+            let c = b.1[x];
+            if c != tile {
+                difference.push(((x, a.0), tile))
+            }
+        }
+        if difference.len() == 1 {
+            let mut tried_map = map.clone();
+            let new_char = if difference[0].1 == '#' {
+                '.'
+            } else {
+                '#'
+            };
+            let ((x, y), _) = difference[0];
+            tried_map[y][x] = new_char;
+            all_mirrored_at.push(mirrored_at(&tried_map));
+        }
+    }
+
+    all_mirrored_at
+        .iter()
+        .find(|&m| m.is_some())
+        .and_then(|m| m.clone())
+}
+
+fn mirrored_at_2(grid: &Vec<Vec<char>>) -> Option<usize> {
+    (1..grid.len()).find(|&offset| {
+        let half1 = grid.iter().take(offset).rev();
+        let half2 = grid.iter().skip(offset);
+        let combined = half1.zip(half2); // the shortest half determines how long this is!
+        let differences: usize = combined
+            .map(|(row1, row2)| row1.iter().zip(row2.iter()).filter(|(a, b)| a != b).count())
+            .sum();
+
+        differences == 1
+    })
+}
+
+pub fn part_two(input: &str) -> Option<usize> {
+    let maps_list: Vec<Vec<Vec<char>>> = input.split("\n\n").map(|m| m
+        .lines()
+        .map(|r| r.chars()
+            .collect())
+        .collect()
+    )
+        .collect();
+    let maps_2d: Vec<Array2D<char>> = input.split("\n\n").clone().map(|m| get_map(m)).collect();
+    let mut r = 0;
+    for (x, grid) in maps_list.iter().enumerate() {
+        if let Some(h_offset) = mirrored_at_2(grid) {
+            r += h_offset * 100;
+        }
+        let cols: Array2D<char> = maps_2d[x].clone();
+        if let Some(v_offset) = mirrored_at_2(&cols.as_columns()) {
+            r += v_offset
+        }
+    }
+
+    Some(r)
 }
 
 #[cfg(test)]
@@ -65,6 +130,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(400));
     }
 }
