fbb75dbe6d66efb56a6796db0cbc72fa81f73a11
billo <ateilhet@gmail.com>
2024-01-12 14:33:05 +0100

    day 14 part 2

--- src/bin/14.rs
+++ src/bin/14.rs
@@ -2,7 +2,7 @@ use array2d::Array2D;
 
 use advent_of_code::get_map;
 
-use crate::Direction::{East, South, West};
+use crate::Direction::{East, North, South, West};
 
 advent_of_code::solution!(14);
 
@@ -58,7 +58,7 @@ fn slide_south(x: usize, y: usize, map: &Vec<Vec<char>>) -> (usize, usize) {
 }
 
 fn slide_west(x: usize, y: usize, map: &Vec<Vec<char>>) -> (usize, usize) {
-    let row = map.get(x);
+    let row = map.get(y);
     let checked_x = x.checked_sub(1);
     if row.is_none() || checked_x.is_none() {
         return (x, y);
@@ -68,22 +68,55 @@ fn slide_west(x: usize, y: usize, map: &Vec<Vec<char>>) -> (usize, usize) {
         return (x, y);
     }
     match c.unwrap() {
-        '.' => slide_west(x, checked_x.unwrap(), map),
+        '.' => slide_west(checked_x.unwrap(), y, map),
         _ => (x, y)
     }
 }
 
+fn slide_east(x: usize, y: usize, map: &Vec<Vec<char>>) -> (usize, usize) {
+    let row = map.get(y);
+    let checked_x = x.checked_add(1);
+    if row.is_none() || checked_x.is_none() {
+        return (x, y);
+    }
+    let c = row.unwrap().get(checked_x.unwrap());
+    if c.is_none() {
+        return (x, y);
+    }
+    match c.unwrap() {
+        '.' => slide_east(checked_x.unwrap(), y, map),
+        _ => (x, y)
+    }
+}
+
+fn map_weight(map: &Array2D<char>) -> usize {
+    map.as_rows()
+        .iter()
+        .rev()
+        .enumerate()
+        .map(|(i, row)| {
+            let round_rocks = row.iter().filter(|tile| **tile == 'O').count();
+            round_rocks * (i + 1)
+        })
+        .sum()
+}
+
 pub fn part_two(input: &str) -> Option<usize> {
     let mut map = get_map(input);
-    let mut r = 0;
-    cycle(&mut map, South);
+    let mut buffer = vec![map.clone()];
+    for _ in 0..1_000_000_000 {
+        cycle(&mut map, North);
+        if let Some(idx) = buffer.iter().position(|x| x == &map) {
+            let cycle_len = buffer.len() - idx;
+            let final_idx = idx + (1_000_000_000 - idx) % cycle_len;
 
-    for r in map.as_rows() {
-        println!("{:?}", r)
+            return Some(map_weight(&buffer[final_idx]));
+        } else {
+            buffer.push(map.clone());
+        }
     }
 
-
-    Some(r)
+    None
 }
 
 enum Direction {
@@ -100,12 +133,14 @@ fn update_map(map: &mut Array2D<char>, x: usize, y: usize, slid_x: usize, slid_y
 
 fn cycle(map: &mut Array2D<char>, direction: Direction) {
     match direction {
-        Direction::North => {
-            let cols: Vec<Vec<char>> = map.as_columns();
-            for (x, col) in cols.iter().enumerate() {
+        North => {
+            let mut cols: Vec<Vec<char>> = map.as_columns();
+            for (x, col) in cols.clone().iter().enumerate() {
                 for (y, c) in col.iter().enumerate() {
                     if c == &'O' {
                         let (slid_x, slid_y) = slide_north(x, y, &cols);
+                        cols[x][y] = '.';
+                        cols[slid_x][slid_y] = 'O';
                         update_map(map, x, y, slid_x, slid_y);
                     }
                 }
@@ -113,11 +148,13 @@ fn cycle(map: &mut Array2D<char>, direction: Direction) {
             cycle(map, West)
         }
         West => {
-            let rows: Vec<Vec<char>> = map.as_rows();
-            for (y, row) in rows.iter().enumerate() {
+            let mut rows: Vec<Vec<char>> = map.as_rows();
+            for (y, row) in rows.clone().iter().enumerate() {
                 for (x, c) in row.iter().enumerate() {
                     if c == &'O' {
                         let (slid_x, slid_y) = slide_west(x, y, &rows);
+                        rows[y][x] = '.';
+                        rows[slid_y][slid_x] = 'O';
                         update_map(map, x, y, slid_x, slid_y);
                     }
                 }
@@ -128,7 +165,7 @@ fn cycle(map: &mut Array2D<char>, direction: Direction) {
             let mut cols: Vec<Vec<char>> = map.as_columns();
             for (x, col) in cols.clone().iter().enumerate() {
                 let mut y = col.len() - 1;
-                while y >= 0 {
+                loop {
                     let c = col[y];
                     if c == 'O' {
                         let (slid_x, slid_y) = slide_south(x, y, &cols);
@@ -144,7 +181,25 @@ fn cycle(map: &mut Array2D<char>, direction: Direction) {
             }
             cycle(map, East)
         }
-        East => {}
+        East => {
+            let mut rows: Vec<Vec<char>> = map.as_rows();
+            for (y, row) in rows.clone().iter().enumerate() {
+                let mut x = row.len() - 1;
+                loop {
+                    let c = row[x];
+                    if c == 'O' {
+                        let (slid_x, slid_y) = slide_east(x, y, &rows);
+                        rows[y][x] = '.';
+                        rows[slid_y][slid_x] = 'O';
+                        update_map(map, x, y, slid_x, slid_y);
+                    }
+                    match x.checked_sub(1) {
+                        Some(_) => x -= 1,
+                        _ => break
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -161,6 +216,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(64));
     }
 }
